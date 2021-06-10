28c4ebd12136d1da01276ebbb065c7ba5b6ed3a9
Nathan Louie <nxlouie@umich.edu>
2021-06-10 02:31:47 -0400

    the vision is realized

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from 'react';
 import styled from 'styled-components';
 import morelHeadImg from './img/morel-head.png';
 import morelStemImg from './img/morel-stem.png';
+import sulfurHeadImg from './img/sulfur-head.png';
+import sulfurStemImg from './img/sulfur-stem.png';
+import flyHeadImg from './img/fly-head.png';
+import flyStemImg from './img/fly-stem.png';
+import spaceImg from './img/bg-space.png';
 import urbanImg from './img/bg-urban.png';
+import blankImg from './img/bg-blank.png';
 
 const Container = styled.div`
   display: inline-block;
@@ -14,7 +20,7 @@ const Container = styled.div`
   padding-left: 3rem;
 `;
 const ComponentImg = styled.img`
-  height: 11rem;
+  height: 14rem;
   object-fit: cover;
   width: 14rem;
 `;
@@ -28,8 +34,7 @@ class ComponentCard extends React.Component {
     const { imgUrl, id, selectHandler } = this.props;
     return (
       <Container onClick={() => selectHandler(id)}>
-        <ComponentImg src= {imgUrl}>
-        </ComponentImg>
+        <ComponentImg src= {imgUrl}/>
       </Container>
     )
   }
@@ -49,10 +54,10 @@ class ComponentRow extends React.Component {
     const { title, componentCardItems, selectHandler } = this.props;
     return (
       <>
-        <Title><td>{title}</td></Title>
-        <tr>
+        <td><Title>{title}</Title></td>
+        <div>
           {componentCardItems.map(card => <ComponentCard imgUrl={card.imgUrl} id={card.id} selectHandler={selectHandler}/>)}
-        </tr>
+        </div>
       </>
     )
   }
@@ -64,11 +69,11 @@ const MushroomHeadsData = [
     id:0,
   },
   {
-    imgUrl:'https://media.npr.org/assets/img/2018/02/02/enoki-promo_wide-5f65ced60025d4ef144f006496ccf5d00ca04021-s600-c85.webp',
+    imgUrl: sulfurHeadImg,
     id:1,
   },
   {
-    imgUrl:'https://civileats.com/wp-content/uploads/2021/03/210311-doug-bierend-mycotopia-mushrooms-fungi-climate-local-food-nutrition-6-king-bolete-credit-bernard-spragg.jpg',
+    imgUrl: flyHeadImg,
     id:2,
   },
 ]
@@ -79,29 +84,73 @@ const MushroomStemsData = [
     id:3,
   },
   {
-    imgUrl:'https://devour.asia/wp-content/uploads/2020/01/fungus-1194380_1280.jpg',
+    imgUrl: sulfurStemImg,
     id:4,
   },
   {
-    imgUrl:'https://devour.asia/wp-content/uploads/2020/01/fungus-1194380_1280.jpg',
+    imgUrl: flyStemImg,
     id:5,
   },
 ]
 
 const MushroomBackgroundData = [
   {
-    imgUrl:urbanImg,
+    imgUrl: urbanImg,
     id:6,
   },
   {
-    imgUrl:'https://media.nature.com/w700/magazine-assets/d41586-019-03614-0/d41586-019-03614-0_17409632.jpg',
+    imgUrl: spaceImg,
     id:7,
   },
   {
-    imgUrl:'https://media.nature.com/w700/magazine-assets/d41586-019-03614-0/d41586-019-03614-0_17409632.jpg',
+    imgUrl: blankImg,
     id:8,
   },
 ]
+
+const OverlayContainer = styled.div`
+  position: relative;
+  display: inline;
+`;
+const OverlayComponentImg = styled(ComponentImg)`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
+
+class ResultMushroom extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {selectedHeadId, selectedStemId, selectedBackgroundId } = this.props;
+    var chosenHeadObj = null;
+    var chosenStemObj = null;
+    var chosenBgObj = null;
+
+    if(selectedHeadId !== null) {
+      chosenHeadObj = MushroomHeadsData.filter(obj => {return obj.id === selectedHeadId})[0];
+    }
+    if(selectedStemId !== null) {
+      chosenStemObj = MushroomStemsData.filter(obj => {return obj.id === selectedStemId})[0];
+    }
+    if(selectedBackgroundId !== null) {
+      chosenBgObj = MushroomBackgroundData.filter(obj => {return obj.id === selectedBackgroundId})[0];
+    }
+
+    return (
+      <div className="Result">
+      <td><Title>{"your mushroom"}</Title></td>
+      <OverlayContainer>
+        {chosenStemObj !== null && <OverlayComponentImg src= {chosenStemObj.imgUrl}/>}
+        {chosenHeadObj !== null && <OverlayComponentImg src= {chosenHeadObj.imgUrl}/>}
+        {chosenBgObj !== null && <ComponentImg src={chosenBgObj.imgUrl}/>}
+      </OverlayContainer>
+    </div>
+    );
+  }
+}
 
 const AppTitle = styled.h1`
   padding-bottom: 0.5rem;
@@ -116,9 +165,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedHeadId: -1,
-      selectedStemId: -1,
-      selectedBackgroundId: -1,
+      selectedHeadId: null,
+      selectedStemId: null,
+      selectedBackgroundId: 8,
     };
 
     this.handleSelectHead = this.handleSelectHead.bind(this);
@@ -147,20 +196,13 @@ class App extends React.Component {
   render() {
     return (
       <AppDiv>
-        <AppTitle><td>welcome to mushroom builder</td></AppTitle>
+        <AppTitle>welcome to mushroom builder</AppTitle>
         <div className="Selection">
           <ComponentRow title="select head" componentCardItems={MushroomHeadsData} selectHandler={this.handleSelectHead}/>
           <ComponentRow title="select stem" componentCardItems={MushroomStemsData} selectHandler={this.handleSelectStem}/>
           <ComponentRow title="select background" componentCardItems={MushroomBackgroundData} selectHandler={this.handleSelectBackground}/>
         </div>
-        <div className="Result">
-          <Title><td>{"your mushroom"}</td></Title>
-          {this.state.selectedHeadId}
-          <br/>
-          {this.state.selectedStemId}
-          <br/>
-          {this.state.selectedBackgroundId}
-        </div>
+        <ResultMushroom selectedHeadId={this.state.selectedHeadId} selectedStemId={this.state.selectedStemId} selectedBackgroundId={this.state.selectedBackgroundId}/>
       </AppDiv>
     );
   }
